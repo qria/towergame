4c5b6d136ed9c275f889d8dd14fca5424edf1f82
Qria <qria@qria.net>
2017-09-08 18:37:08 +0900

    Add basic layout and style

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
+
+
 app = Flask(__name__)
+app.jinja_env.auto_reload = True  # Don't cache templates
+
 
 @app.route('/')
-def hello_world():
+def redirect_to_index():
+	return redirect(url_for('index'))
+
+@app.route('/index.htm')
+def index():
     return render_template('index.html')
+
+
+@app.route('/frontdoor.htm')
+def front_door():
+    return render_template('frontdoor.html')
